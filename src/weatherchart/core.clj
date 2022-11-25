781fc997f335bf40b749423351c9a727901cbaee
Eddie Lebow <elebow@users.noreply.github.com>
2022-11-25 00:39:22 -0500

    Colorize chart titles

--- src/weatherchart/core.clj
+++ src/weatherchart/core.clj
@@ -13,14 +13,14 @@
                   "</style>
                 </head>
                 <body>
-                  <p class='chart-title'>temperature and apparent temperature</p>"
+                  <p class='chart-title'><span style='color:purple'>temperature</span> and <span style='color:red'>apparent temperature</span></p>"
                   (weatherchart.chart/render-chart {:min -10 :max 40 :step 10} [{:points (weatherchart.data/points-for-layer "temperature") :color "purple"}
                                                                                 {:points (weatherchart.data/points-for-layer "apparentTemperature") :color "red"}])
-                  "<p class='chart-title'>relative humidity</p>"
+                  "<p class='chart-title' style='color:green'>relative humidity</p>"
                   (weatherchart.chart/render-chart {:min 0 :max 100 :step 10} [{:points (weatherchart.data/points-for-layer "relativeHumidity") :color "green"}])
-                  "<p class='chart-title'>probability of precipitation</p>"
+                  "<p class='chart-title' style='color:blue'>probability of precipitation</p>"
                   (weatherchart.chart/render-chart {:min 0 :max 100 :step 10} [{:points (weatherchart.data/points-for-layer "probabilityOfPrecipitation") :color "blue"}])
-                  "<p class='chart-title'>rain amount</p>"
+                  "<p class='chart-title' style='color:darkgreen'>rain amount</p>"
                   (weatherchart.chart/render-chart {:min 0 :max 20 :step 10} [{:points (weatherchart.data/points-for-layer "quantitativePrecipitation") :color "darkgreen"}])
                 "</body></html>")))
 
