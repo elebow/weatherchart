c5f078d5587a5881b381bcbffccfe4579f2888fa
Eddie Lebow <elebow@users.noreply.github.com>
2022-11-25 01:46:26 -0500

    Show units in chart titles

--- src/weatherchart/core.clj
+++ src/weatherchart/core.clj
@@ -13,14 +13,14 @@
                   "</style>
                 </head>
                 <body>
-                  <h2 class='chart-title'><span style='color:var(--color-temperature)'>temperature</span> and <span style='color:var(--color-apparent-temperature)'>apparent temperature</span></h2>"
+                  <h2 class='chart-title'><span style='color:var(--color-temperature)'>temperature (°C)</span> and <span style='color:var(--color-apparent-temperature)'>apparent temperature (°C)</span></h2>"
                   (weatherchart.chart/render-chart {:min -10 :max 40 :step 10} [{:points (weatherchart.data/points-for-layer "temperature") :colorname "temperature"}
                                                                                 {:points (weatherchart.data/points-for-layer "apparentTemperature") :colorname "apparent-temperature"}])
-                  "<h2 class='chart-title' style='color:var(--color-relative-humidity)'>relative humidity</h2>"
+                  "<h2 class='chart-title' style='color:var(--color-relative-humidity)'>relative humidity (%)</h2>"
                   (weatherchart.chart/render-chart {:min 0 :max 100 :step 10} [{:points (weatherchart.data/points-for-layer "relativeHumidity") :colorname "relative-humidity"}])
-                  "<h2 class='chart-title' style='color:var(--color-probability-of-precipitation)'>probability of precipitation</h2>"
+                  "<h2 class='chart-title' style='color:var(--color-probability-of-precipitation)'>probability of precipitation (%)</h2>"
                   (weatherchart.chart/render-chart {:min 0 :max 100 :step 10} [{:points (weatherchart.data/points-for-layer "probabilityOfPrecipitation") :colorname "probability-of-precipitation"}])
-                  "<h2 class='chart-title' style='color:var(--color-quantitative-precipitation)'>rain amount</h2>"
+                  "<h2 class='chart-title' style='color:var(--color-quantitative-precipitation)'>rain amount (mm)</h2>"
                   (weatherchart.chart/render-chart {:min 0 :max 20 :step 10} [{:points (weatherchart.data/points-for-layer "quantitativePrecipitation") :colorname "quantitative-precipitation"}])
                 "</body></html>")))
 
@@ -29,8 +29,9 @@
 
 
 
-   ;"snowfallAmount"
+   ;"snowfallAmount" (mm)
    ;"pressure"
-   ;"skyCover"
+   ;"skyCover" (%)
    ;"weather" ; non-numeric
-   ;"windSpeed"
+   ;"windSpeed" (km/h)
+   ;"hazards"
