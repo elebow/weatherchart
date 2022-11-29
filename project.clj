0f491d38fdabb672eb06bcfb187ee53ab9b7c81f
Eddie Lebow <elebow@users.noreply.github.com>
2022-11-29 00:41:02 -0500

    Change license to AGPL-3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject weatherchart "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GPL-3.0"
-            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
+  :license {:name "AGPL-3.0"
+            :url "https://www.gnu.org/licenses/agpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "2.4.0"]
                  [clojure.java-time "1.1.0"]
