135c988703c76ee00a1c33b65b1a942d3985f4e0
Eddie Lebow <elebow@users.noreply.github.com>
2022-11-29 00:39:12 -0500

    Highlight current hour in weatherconditions chart

--- src/weatherchart/weatherconditions.clj
+++ src/weatherchart/weatherconditions.clj
@@ -18,5 +18,6 @@
   (str "<svg viewBox='0 0 " (weatherchart.chart/x-for-datetime weatherchart.data/latest-datetime) " " (+ 15 weatherchart.chart/chart-height) "' height='400px' xmlns='http://www.w3.org/2000/svg'>"
         weatherchart.chart/render-grid-vert-midnights
         weatherchart.chart/render-grid-vert-hours
+        weatherchart.chart/render-current-hour-highlight
         render-weather-elements
       "</svg>"))
